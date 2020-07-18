16c3794772112ff03fe66aeea17a6baf22728b1b
gaojun <gaojun168@163.com>
2020-07-18 13:29:41 +0800

    2

--- src/p83/Test82.java
+++ src/p83/Test82.java
@@ -4,7 +4,7 @@ public class Test82 {
 
 	public static void main(String[] args) {
 		// ºº×Ö²âÊÔ
-		System.out.println("ºº×Ö²âÊÔ   ºº×Ö²âÊÔ   ºº×Ö²âÊÔ");
+		System.out.println("ºº×Ö²âÊÔ1   ºº×Ö²âÊÔ1   ºº×Ö²âÊÔ1");
 
 	}
 
